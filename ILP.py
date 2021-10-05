04012b3970cfa8ed363e8a182896cac9eb7fcd74
Andrew Cantrell <andyroocantrell@gmail.com>
2021-10-04 17:54:14 -0700

    Finished it I think?

--- ILP.py
+++ ILP.py
@@ -1,3 +1,4 @@
+
 import numpy.linalg
 import json
 
@@ -8,44 +9,37 @@ def solve(b, a, s, ct):
         ls = [0] * n
         for i in range(n):
             ls[i] = b[i] - s[i]
-        print(ls)
         xvals = numpy.linalg.solve(a, ls)
 
-
-        # ainv = numpy.linalg.inv(a)
-        # multiply left side by inverse of a to find values of x and return
-        # xvals = numpy.dot(ainv, ls)
-        if not isinstance(xvals[0], list):
-            print("Verifying solution")
-            print("True" if (numpy.dot(a, xvals) is b) else "False")
-            return xvals
-
-        else:
+        '''
+        Another way to solve this system manually
+        ainv = numpy.linalg.inv(a)
+        multiply left side by inverse of a to find values of x and return
+        xvals = numpy.dot(ainv, ls)
+        '''
+        if isinstance(xvals[0], list):
             biggest = xvals[0]
             for j in range(1, len(xvals)):
                 if numpy.dot(ct, xvals[j]) > numpy.dot(ct, biggest):
                     biggest = xvals[j]
             print("Verifying solution")
             print("True" if (numpy.dot(a, xvals) is b) else "False")
-            return biggest
+            xvals = biggest
+
+        print("Verifying solution")
+        ax = numpy.dot(a, xvals).tolist()
+        ax = [round(num) for num in ax]
+        print(ax, "  is the dot product of solution with original matrix")
+        print(ls, "  is the calculated b - s")
+        print("Valid solution" if ax == ls else "Invalid Solution")
+        return xvals
 
 
     except ValueError:
-        print("Opps! The matrix isn't singular")
-
-    #return numpy.dot(ls, ct)
+        print("Opps! The matrix isn't solvable")
 
 
 def main():
-    # b = [5.0, 12.9, 2.0]
-    #
-    # a = [[3.2, 8.7, 5.9],
-    #      [2.4, 3.1, 1.1],
-    #      [9.7, 6.1, 0.3]]
-    #
-    # s = [3.7, 3.8, 1.0]
-    #
-    # ct = [8.2, 9.7, 1.1]
     file = json.load(open('matrix2.json'))
     matrices = file['Matrix']
     a = matrices['a']
@@ -55,10 +49,9 @@ def main():
     answers = solve(b, a, s, ct)
     out = "Maximum value answers is "
     for num in answers:
-        out += str(num) + "   "
+        out += str(round(num, 5)) + "   "
     print(out)
-    print(str(numpy.dot(ct, answers)) + "   is the value of cT.X")
-
+    print(str(round(numpy.dot(ct, answers), 5)) + "   is the value of cT and X dot product")
 
 
 if __name__ == '__main__':
--- LP.py
+++ LP.py
@@ -10,28 +10,33 @@ def solve(b, a, s, ct):
         ls = [0] * n
         for i in range(n):
             ls[i] = float(Decimal(b[i]) - Decimal(s[i]))
-
         xvals = numpy.linalg.solve(a, ls)
 
-        if not isinstance(xvals[0], list):
-            print("Verifying solution")
-            print("True" if (numpy.dot(a, xvals) is b) else "False")
-            return xvals
-
-        else:
+        '''
+                Another way to solve this system manually
+                ainv = numpy.linalg.inv(a)
+                multiply left side by inverse of a to find values of x and return
+                xvals = numpy.dot(ainv, ls)
+                '''
+        if isinstance(xvals[0], list):
             biggest = xvals[0]
             for j in range(1, len(xvals)):
                 if numpy.dot(ct, xvals[j]) > numpy.dot(ct, biggest):
                     biggest = xvals[j]
             print("Verifying solution")
-            print("True" if (numpy.dot(a, xvals) is b)  else "False")
-            return biggest
+            print("True" if (numpy.dot(a, xvals) is b) else "False")
+            xvals = biggest
 
+        print("Verifying solution")
+        ax = numpy.dot(a, xvals).tolist()
+        ax = [round(num) for num in ax]
+        print(ax, "  is the dot product of solution with original matrix")
+        print(ls, "  is the calculated b - s")
+        print("Valid solution" if ax == ls else "Invalid Solution")
+        return xvals
 
     except ValueError:
-        print("Opps! The matrix isn't singular")
-
-    #return numpy.dot(ls, ct)
+        print("Opps! The matrix isn't solvable")
 
 
 def main():
@@ -44,9 +49,9 @@ def main():
     answers = solve(b, a, s, ct)
     out = "Maximum value answers is "
     for num in answers:
-        out += str(num) + "   "
+        out += str(round(num, 5)) + "   "
     print(out)
-    print(str(numpy.dot(ct, answers)) + "   is the value of cT.X")
+    print(str(round(numpy.dot(ct, answers), 5)) + "   is the value of cT and X dot product")
 
 
 if __name__ == '__main__':
